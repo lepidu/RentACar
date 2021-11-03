37f05cfff35c7b2c96303d0a575061a96cf1e2fa
lepidu <lepidu@live.com>
2021-11-03 05:16:41 -0600

    StudentName

--- src/rentacar/Driver.java
+++ src/rentacar/Driver.java
@@ -1,7 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Leisly Alitzel Pino Duran 2020303
+ * https://github.com/lepidu/RentACar
  */
 package rentacar;
 
@@ -16,23 +15,9 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-// INCLUDE HERE THE LINK TO THE GIT REPOSITORY AS A COMMENT!!!!
-
-
-// INCLUDE HERE THE FULL NAME AND STUDENT NUMBER OF BOTH TEAM MEMBERS 
-// AS A COMMENT!!!!
-
-
-
-/**
- *
- * @author apont
- */
 public class Driver {
 
     /**
-     * Do not change the signature of the main method!
-     * 
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
